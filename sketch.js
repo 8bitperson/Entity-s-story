00c1c4def6fbd00b13bdf2d945152564b6f7c846
akhileshgupta <akhileshone@gmail.com>
2021-11-28 21:18:38 -0500

    entity-s-story(UTD)

--- sketch.js
+++ sketch.js
@@ -9,6 +9,8 @@ var randblu
 var randgreen
 var groupvelocity = -12
 var levels = 1
+var i = 1
+var screen_saver
 alert("Don't touch the ground!")
 alert("dont go to far up")
 alert("space to super jump and up key for mini jump")
@@ -31,16 +33,27 @@ function setup() {
 
 function draw() {
   //background(randred,randblu,randgreen);
-  background(200,50,100)
+  background(200,100,150)
   level()
   drawSprites()
-  textSize(34)
-  text("level:" + levels,windowWidth/2,40)
-    
+  
+  
+  if (keyDown("r")) {
+    window.location.reload();
+  }
+   
+
   if (gamestate == "play"){
       score = Math.round(frameCount/3)
+      level();
+      textSize(34)
+      text("level:" + levels,windowWidth/2,20);
+      //textSize(34)
+      //text("level:" + levels,windowWidth/2,40);
+      //level();
       if(a.collide(invisibleGround) || a.y < 0 || a.x < 0){
-        gamestate = "end"
+      
+      gamestate = "end"
        }
       objective()
       if(keyDown("space")){
@@ -58,17 +71,27 @@ function draw() {
       }
       a.collide(obstaclegroup) 
 
-      if(levels >> 9){
+      if(levels  > 9){
         gamestate = "game done"
       }
    }else if(gamestate == "end"){
-    alert("score:"+score+"      To continue please reload page and press ok")
+     if(gamestate == "end" && i == 1 ){
+      alert("score:"+score+"      To continue Please press ok and then press R ")
+      i = i + 1
+     }
     
-  }else if (levels >> 9) {
+    screen_saver =  createSprite(100,0,1,1)
+    if (keyDown("r")) {
+      window.location.reload();
+    }
+    
+  }else if (levels > 9) {
     obstaclegroup.visible = false
     a.y = windowHeight/2
     a.velocityX = 1
     var portal = createSprite(windowWidth,windowHeight/2,20,windowHeight)
+    gamestate = "game done"
+    score = 0 
   }
   a.collide(invisibleGround)
   
@@ -89,13 +112,12 @@ function objective(){
     } 
 }
 function level(){
-  if(frameCount %  350 === 0){
-     randred = Math.round(random(0,300));
-     randblu = Math.round(random(0,300));
-     randgreen = Math.round(random(0,300));
-     groupvelocity = groupvelocity - 2
-     if(levels >> 9){
-      levels = levels + 1
-     }
+  if(frameCount %  177 === 0){
+     //randred = Math.round(random(0,300));
+     //randblu = Math.round(random(0,300));
+     //randgreen = Math.round(random(0,300));
+     groupvelocity = groupvelocity - 2 
+     levels = levels + 1
+     //alert("This works")
   }  
 } 
